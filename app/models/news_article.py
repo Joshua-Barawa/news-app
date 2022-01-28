66c20717935b4fcee753ad1397c9dbfa8977e162
Joshua-Barawa <joshua.barawa@student.moringaschool.com>
2022-01-28 16:31:51 +0300

    added two models classes news_source and news_Article

---
+++ app/models/news_article.py
@@ -0,0 +1,8 @@
+class Article:
+    def __init__(self, name, author, title, description, urlToImage, publishedAt):
+        self.name = name
+        self.author = author
+        self.title = title
+        self.description = description
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
---
+++ app/requests.py
@@ -0,0 +1,4 @@
+from configuration.config import API_KEY
+
+MOVIE_API_KEY = API_KEY
+
