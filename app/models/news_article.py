74b4c5ab8223b49b6eb5b332b5679ebb2fda7029
Joshua-Barawa <joshua.barawa@student.moringaschool.com>
2022-01-30 12:53:39 +0300

    created cards for every news_article

--- app/models/news_article.py
+++ app/models/news_article.py
@@ -1,8 +1,9 @@
 class Article:
-    def __init__(self, source, author, title, description, urlToImage, publishedAt):
+    def __init__(self, source, author, title, description, urlToImage, url, publishedAt):
         self.source = source
         self.author = author
         self.title = title
         self.description = description
         self.urlToImage = urlToImage
+        self.url = url
         self.publishedAt = publishedAt
--- app/requests.py
+++ app/requests.py
@@ -7,12 +7,12 @@ News_Article = news_article.Article
 
 
 def get_news():
-    request = requests.get('https://newsapi.org/v2/everything?q=bitcoin&apiKey=951ac88dc85d4aa3a2fc00d764d0f73c'
+    request = requests.get('https://newsapi.org/v2/everything?q=technology&apiKey=951ac88dc85d4aa3a2fc00d764d0f73c'
                            .format(MOVIE_API_KEY))
     response = json.loads(request.content)
     news = []
     for new in response['articles']:
-        new = News_Article(new['source'], new['author'], new['title'], new['description'], new['urlToImage'], new['publishedAt'])
+        new = News_Article(new['source'], new['author'], new['title'], new['description'], new['urlToImage'], new['url'], new['publishedAt'])
         news.append(new)
 
     print(news)
